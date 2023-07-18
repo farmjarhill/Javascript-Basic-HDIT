cceff01d9aca3b6d51e5792d772795c1b8d0812d
pgLinhh <pghaothuat@gmail.com>
2023-07-18 22:36:02 +0700

    source chapter 2, video6

---
+++ video6/video6.js
@@ -0,0 +1,27 @@
+console.log('Hello world from HTML'.toUpperCase());
+
+let a = 5  // int, float, long, double, ... in java but in javascript there is only Numbers.
+let b = 6
+let c = 6.9
+let d = '5'
+console.log(a + b)
+console.log(a + c)
+
+console.log(a + d) // co moi toan tu cong la javascript covert thanh string
+console.log(a - d)
+console.log(a * d)
+console.log(a === d, "because", 'a: ', typeof a, 'and', 'd: ', typeof d)
+
+let x = 100
+let y = 200
+
+console.log('x + y = ', '100 + 200 = ', x + y )
+
+// Convert string to Number
+let e = +d; // let e = Number(d) nhu nhau.
+console.log('e value:', e, 'type:', typeof e)
+console.log(a === e, "because", 'a: ', typeof a, 'and', 'e: ', typeof e)
+
+
+
+
