b44aca6fd47444563a8417ed6be17991ad53c78f
pgLinhh <pghaothuat@gmail.com>
2023-08-02 12:01:23 +0700

    source chapter 3, video16

---
+++ video16/video16.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML');
+
+//Input parameter
+function sum(a, b){
+    return a + b;
+}
+let c = sum(10, 20)
+console.log('sum a + b =', sum(6, 9))
+console.log('sum c', c)
