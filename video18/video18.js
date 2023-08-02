df9cc85afb6aae1e5ae50bda6b3a0347f11d5b57
pgLinhh <pghaothuat@gmail.com>
2023-08-02 17:17:38 +0700

    source chapter 4, video18

---
+++ video18/video18.js
@@ -0,0 +1,18 @@
+console.log('Hello world from HTML');
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+// Arrow function sẽ giúp học React dễ dàng hơn
+sum = (a, b) => {
+    return a + b;
+}
+console.log('Check sum:', sum(6, 9));
+
+
+hello = () => {
+    return "Hello FarmJarHill!";
+}
+console.log(hello());
+
