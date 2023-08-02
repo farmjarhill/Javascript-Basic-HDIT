0193f45ed1987be772853e8820f2477bdc97a105
pgLinhh <pghaothuat@gmail.com>
2023-08-02 10:52:10 +0700

    source chapter 3, video13

---
+++ video13/video13.js
@@ -0,0 +1,38 @@
+console.log('Hello world from HTML');
+
+let arrTop4 = ['Live', 'Chelsea', 'MU', 'Man City'];
+
+let i = 0;
+while (i < arrTop4.length) {
+    // if else condition
+    if (arrTop4[i].length === 2) {
+        console.log('Top club:', i + 1, arrTop4[i]);
+    } else if (arrTop4[i].length === 4) {
+        console.log('Top club:', i + 1, arrTop4[i]);
+    } else {
+
+    }
+    //console.log('Top club:', i + 1, arrTop4[i]);
+    i++;
+}
+
+// BREAK CODE:
+// while (i < arrTop4.length) {
+//     console.log('>>>> check i:', i);
+//     if (arrTop4[i] === 'Chelsea') {
+//         console.log('Found it:', arrTop4[i]);
+//         break;
+//     }
+//     i++;
+// }
+
+// CONTINUE CODE:
+// while (i < arrTop4.length) {
+//     i++;
+//     if (arrTop4[i] === 'Chelsea') {
+//         console.log('Found it:', arrTop4[i]);
+//         continue;
+//     }
+//     console.log('>>>> check i:', i);
+    
+// }
